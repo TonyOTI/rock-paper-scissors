641848949a4b1c7d70bded16fc493d982c1a832c
tony <waznyt@gmail.com>
2023-07-15 15:17:20 -0400

    Create getComputerChoice() in rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,8 @@
 // rock paper and a scissors, oh yaeh
+
+function getComputerChoice() {
+    let arr = ['Rock', 'Paper', 'Scissors'];
+    return arr[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
